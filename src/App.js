9c34a39262f6d3f228985c61d403ff260fa1dbcb
madisonmincevich <maddiebooldridge@gmail.com>
2022-03-11 10:56:39 -0800

    Basic HTML setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <h1>Weather App</h1>
+        <Weather defaultCity="London" />
         <footer>
           <a
             href="https://github.com/madisonmincevich/weather-react-app"
