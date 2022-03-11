cc797e05c0ee93e4e60b2038cf0c3b2db6a74667
madisonmincevich <maddiebooldridge@gmail.com>
2022-03-11 10:31:34 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <p>
-        <a
-          href="https://github.com/madisonmincevich/weather-react-app"
-          alt=""
-          target="_blank"
-        >
-          Open sourced code
-        </a>{" "}
-        by Maddie Mincevich
-      </p>
+      <div className="container">
+        Weather App
+        <footer>
+          <a
+            href="https://github.com/madisonmincevich/weather-react-app"
+            alt=""
+            target="_blank"
+          >
+            Open sourced code
+          </a>{" "}
+          by Maddie Mincevich
+        </footer>
+      </div>
     </div>
   );
 }
