2891a009fd233232ee6d75b6a02e4dc82013f113
madisonmincevich <maddiebooldridge@gmail.com>
2022-03-11 10:24:44 -0800

    Link to GitHub

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <p>
+        <a
+          href="https://github.com/madisonmincevich/weather-react-app"
+          alt=""
+          target="_blank"
+        >
+          Open sourced code
+        </a>{" "}
+        by Maddie Mincevich
+      </p>
+    </div>
+  );
 }
 
 export default App;
